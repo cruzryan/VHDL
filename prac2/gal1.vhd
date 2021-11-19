9f0113c12b36a9c01ab04c0f2def3d08ba2dd50a
cruzryan <32502734+cruzryan@users.noreply.github.com>
2021-11-19 10:57:17 -0600

    no signals

--- prac2/gal1.vhd
+++ prac2/gal1.vhd
@@ -11,31 +11,13 @@ entity Prac2Sumador is
 		Trigger : out std_logic
 		);
 
-attribute PIN_NUMBERS of Prac2Sumador:entity is
-"Trigger:16 " & "DeepSwitch1(0):2 DeepSwitch1(1):3 DeepSwitch1(2):4 " & 
-"DeepSwitch2(0):5 DeepSwitch2(1):6 DeepSwitch2(2):7 " & 
-"SEG2(0):23 SEG2(1):22 SEG2(2):21 SEG2(3):20 SEG2(4):19 SEG2(5):18 SEG2(6):17"; 
+--attribute PIN_NUMBERS of Prac2Sumador:entity is
+--"Trigger:16 " & "DeepSwitch1(0):2 DeepSwitch1(1):3 DeepSwitch1(2):4 " & 
+--"DeepSwitch2(0):5 DeepSwitch2(1):6 DeepSwitch2(2):7 " & 
+--"SEG2(0):23 SEG2(1):22 SEG2(2):21 SEG2(3):20 SEG2(4):19 SEG2(5):18 SEG2(6):17"; 
 end Prac2Sumador;
 
 architecture impl of Prac2Sumador is
-
---	Primer Bit
-	signal X1 : std_logic;
-	signal Y1 : std_logic;
-	signal S1 : std_logic;
-	signal C1 : std_logic;
-
-	--	Segundo Bit
-	signal X2 : std_logic;
-	signal Y2 : std_logic;
-	signal S2 : std_logic;
-	signal C2 : std_logic;
-
-	--	Tercer Bit
-	signal X3 : std_logic;
-	signal Y3 : std_logic;
-	signal S3 : std_logic;
-	signal C3 : std_logic;
 
 	-- Salida en binario
 	signal salBin: std_logic_vector (0 to 3);
@@ -43,39 +25,17 @@ architecture impl of Prac2Sumador is
 	-- Salida en entero
 	signal sum_int : integer;
 
-
 begin
 	process is
 	begin
 	wait for 1 ns;
 
-	-- Ponemos todo en nuevas variables
-
-	X1 <= DeepSwitch1(0);
-	Y1 <= DeepSwitch2(0);
-
-	X2 <= DeepSwitch1(1);
-	Y2 <= DeepSwitch2(1);
-
-	X3 <= DeepSwitch1(2);
-	Y3 <= DeepSwitch2(2);
-
-
 	-- Sumador 
 
-		C1 <= X1 AND Y1;
-		S1 <= (NOT(X1) AND Y1) OR (X1 AND NOT(Y1));
-
-		C2 <= (C1 AND ((X2 AND NOT(Y2)) OR (NOT(X2) AND Y2))) OR (X2 AND Y2);
-		S2 <= C1 XOR (X2 XOR Y2);
-
-		C3 <= (C2 AND ((X3 AND NOT(Y3)) OR (NOT(X3) AND Y3))) OR (X3 AND Y3);
-		S3 <= C2 XOR (X3 XOR Y3);
-			
-		salBin(3) <= S1;
-		salBin(2) <= S2;
-		salBin(1) <= S3;
-		salBin(0) <= C3;
+		salBin(3) <= (NOT(DeepSwitch1(0)) AND DeepSwitch2(0)) OR (DeepSwitch1(0) AND NOT(DeepSwitch2(0)));
+		salBin(2) <= (DeepSwitch1(0) AND DeepSwitch2(0)) XOR (DeepSwitch1(1) XOR DeepSwitch2(1));
+		salBin(1) <= (((DeepSwitch1(0) AND DeepSwitch2(0)) AND ((DeepSwitch1(1) AND NOT(DeepSwitch2(1))) OR (NOT(DeepSwitch1(1)) AND DeepSwitch2(1)))) OR (DeepSwitch1(1) AND DeepSwitch2(1))) XOR (DeepSwitch1(2) XOR DeepSwitch2(2));
+		salBin(0) <= ((((DeepSwitch1(0) AND DeepSwitch2(0)) AND ((DeepSwitch1(1) AND NOT(DeepSwitch2(1))) OR (NOT(DeepSwitch1(1)) AND DeepSwitch2(1)))) OR (DeepSwitch1(1) AND DeepSwitch2(1))) AND ((DeepSwitch1(2) AND NOT(DeepSwitch2(2))) OR (NOT(DeepSwitch1(2)) AND DeepSwitch2(2)))) OR (DeepSwitch1(2) AND DeepSwitch2(2));
 
 
 	-- Codificador 
