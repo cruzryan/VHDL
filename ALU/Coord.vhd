963cedb0b0b053be8be1c814d76f46cab9b98ca6
cruzryan <32502734+cruzryan@users.noreply.github.com>
2021-12-17 14:05:46 -0600

    fixed coord

--- ALU/Coord.vhd
+++ ALU/Coord.vhd
@@ -8,7 +8,9 @@ entity Coords is
 		--Salida hacia el modulo de operaciones boooleanas
 		BOOLEAN_OUT: out std_logic_vector(2 downto 0);
 		--Salida hacia el modulo Flip and Negate (FN)
-		FN_OUT: out std_logic_vector(1 downto 0)
+		FN_OUT: out std_logic_vector(1 downto 0);
+		--Activar mayor menor igual
+		I: out std_logic
 	);
 end Coords;
 
@@ -19,7 +21,8 @@ architecture impl of Coords is
 		num <= to_integer(unsigned(OPCODE));		
 		process
 		begin
-			--wait for 10 ns;
+			wait for 10 ns;
+			 I <= '0';
 			 if ( num < 6 ) then
 			 	BOOLEAN_OUT(2) <= OPCODE(2);
 			 	BOOLEAN_OUT(1) <= OPCODE(1);
@@ -43,7 +46,11 @@ architecture impl of Coords is
 			 	FN_OUT(0) <= '0';
 			 elsif(num = 8) then
 			 	FN_OUT(1) <= '1';
-			 	FN_OUT(0) <= '1';	
+			 	FN_OUT(0) <= '1';
+			 elsif(num = 9) then
+			 	FN_OUT(1) <= '0';
+			 	FN_OUT(0) <= '0';
+			 	I <= '1'; 	
 			 end if;
 		end process;
 end impl;
